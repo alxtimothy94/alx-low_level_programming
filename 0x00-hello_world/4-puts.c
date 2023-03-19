b04da1d7bbe1c1400f73dbc245fcb044a9f9b780
Timothy_winterbottom <Timothy.winterbottom@outlook.com>
2023-03-19 06:12:43 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
+#include <stdio-h>
 /*
  * prints out a line
  *
+ * main - this the a main function
+ *
  * Return: function always 0
  */
-#include <stdio-h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
