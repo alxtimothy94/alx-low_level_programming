cf97d8a49b2adb362202ed86dbdae583201dd02e
Timothy_winterbottom <Timothy.winterbottom@outlook.com>
2023-03-22 12:07:59 -0700

    test

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,42 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
-*print_times_table - prints the  times table with
-*parameter
-*@n: parameter
-*Return: returns nothing
+*print_to_98 - prints natural
+*numbers from n to 98
+*@n:integer to start by
+*return: returns nothing
 */
-void print_times_table(int n)
+void print_to_98(int n)
 {
-int digit, mult, result;
-if (n <= 15 && n >= 0)
+if (n >= 98)
 {
-for (digit = 0; digit <= n; digit++)
-{
-_putchar('0');
+while (n > 98)
+printf("%d, ", n--);
 
-for (mult = 1; mult <= n; mult++)
+printf("%d\n", n);
+}
+else
 {
-_putchar(',');
-_putchar(' ');
-result = digit * mult;
-if (result <= 99)
-_putchar(' ');
+while (n < 98)
+printf("%d, ", n++);
 
-if (result <= 9)
-_putchar(' ');
-if (result >= 100)
-{
-_putchar((result / 100) + '0');
-_putchar((result / 10) % 10 + '0');
-}
-else if (result <= 99 && result >= 10)
-{
-_putchar((result / 10) + '0');
-}
-_putchar((result % 10) + '0');
-}
-_putchar('\n');
+printf("%d\n", n);
 }
 }
-}
-
